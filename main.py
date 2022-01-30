9d5e7d23315556188fa658774f6285b40d54d85e
Meka9556 <98712962+Meka9556@users.noreply.github.com>
2022-01-31 02:51:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# mmkk
- apt update
+apt update
 $ apt upgrade
 $ pkg install git
 $ pkg install python
 $ pkg install python2
 $ pip install vk
-$ pip install vk_api
+$ pip install vk_api 315245875
 $ git clone https://github.com/XuliGan4eg2006/Vk-Tokenner-.git
 $ cd Vk-Tokenner
 $ pip3 install -r requirements.txt
